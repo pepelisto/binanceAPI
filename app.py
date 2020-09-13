624557db272ca4604f7eed04fefb83fc47e87bd3
pepelisto <martinmoran422@gmail.com>
2020-09-12 22:23:33 -0300

    first commit

---
+++ app.py
@@ -0,0 +1,43 @@
+from flask import Flask, request, jsonify
+import json, config
+from binance.client import Client
+from binance.enums import *
+
+app = Flask(__name__)
+
+client = Client(config.API_KEY, config.API_SECRET, tld='us')
+
+def order(side, quantity, symbol, order_type=ORDER_TYPE_MARKET):
+    try:
+        print(f"sending order {order_type} - {side} {quantity} {symbol}")
+        order = client.create_order(symbol=symbol, side=side, type=order_type, quantity=quantity)
+    except Exception as e:
+        print("an exception occured - {}".format(e))
+        return False
+
+    return order
+
+@app.route('/webhook', methods=['POST'])
+def webhook():
+    data = json.loads(request.data)
+    if data['passphrase'] != config.WEBHOOK_PASSPHRASE:
+        return {
+            "code": "binance warning",
+            "message": "invalid passphrase"
+        }
+    side = data['strategy']['order_action'].upper()
+    quantity = data['strategy']['order_contracts']
+    symbol = data['ticker']
+    order_response = order(side, quantity, symbol)
+    if order_response:
+        return{
+            "code": "success",
+            "message": "order executed"
+        }
+    else:
+        print("order failed")
+        return{
+            "code": "error",
+            "message": "order failed"
+        }
+
---
+++ config.py
@@ -0,0 +1,4 @@
+WEBHOOK_PASSPHRASE = "passphrase"
+
+API_KEY = 'apikey'
+API_SECRET = 'apisecret'
